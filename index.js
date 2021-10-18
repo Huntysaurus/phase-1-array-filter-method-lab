3477da30660677bf0025f76736d598398343a8cb
Jacob <yawjacob@yahoo.com>
2021-10-18 17:38:36 -0500

    commit tree 9f18dc491fc94e15a9267187ef51d06e5322cdaa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr, letter) {
+  return arr.filter(el => el.toLowerCase().startsWith(letter.toLowerCase()))
+}
+
+function fuzzyMatch(arr, letter) {
+  return arr.filter(el => el.startsWith(letter))
+}
+
+function matchName(arr, name) {
+  console.log('-----', name)
+  return arr.filter(function(element) {
+    return element.name === name 
+  })
+}
